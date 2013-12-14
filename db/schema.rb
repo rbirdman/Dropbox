8fc9445c0d93cc47f7fbcfb947c993ca2a2f5787
Josh Lepinski <jlepinski@verisage.us>
2013-12-14 16:07:36 -0700

    added root and path for finding files in directories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131214213639) do
+ActiveRecord::Schema.define(version: 20131214230207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20131214213639) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "path"
   end
 
   add_index "file_items", ["user_id"], name: "index_file_items_on_user_id", using: :btree
@@ -76,6 +77,7 @@ ActiveRecord::Schema.define(version: 20131214213639) do
     t.string   "privilege",      limit: 10
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "root_directory"
   end
 
   add_index "users", ["netid"], name: "UniqueNetId", unique: true, using: :btree
