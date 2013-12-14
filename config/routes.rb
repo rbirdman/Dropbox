c7e2673902870647c59947822a7905427a0cdd6e
Kevin Ostrin <ostrin9@gmail.com>
2013-12-14 14:02:12 -0700

    Header stuff but not quite working

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Src::Application.routes.draw do
   get "users" => "users#user-search"
   get "users/search/:id" => "users#searched"
   get "users/:id" => "users#show"
-  get "users/:id/edit" => "users#edit"
+  get "users/edit" => "users#edit"
 
   get "calendar" => "dropbox#calendar"
   get "calendar/:id" => "dropbox#calendar"
