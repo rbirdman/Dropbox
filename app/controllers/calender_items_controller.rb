56d9f09512bfeba6164dc2c85f46ab9bd554dfac
bchase524 <bchase524@gmail.com>
2013-12-14 20:19:53 -0700

    Calendar Items feature displays completely if params id is okay

--- app/controllers/calender_items_controller.rb
+++ app/controllers/calender_items_controller.rb
@@ -1,23 +1,7 @@
 require 'builder'
 
 class CalenderItemsController < ApplicationController
-	def produce_xml (citems)
-	  File.delete("/assets/calendar-events.xml") if File.exist?("/assets/calendar-events.xml")
-	  file = File.new("/assets/calendar-events.xml", "w")
-	  xml = Builder::XmlMarkup.new(:target=>file, :indent => 2)
-
-		xml.instruct! :xml, :encoding => "UTF-8",  :version => "1.0"
-
-		xml.data do |data|
-			data.event("id"=>c.id, "start_date"=>c.start_datetime, "end_date"=>c.end_datetime, "text"=>c.name, "details"=>"")
-		end
-	  file.close
-	end
-	
 	def calendar
-		Rails.logger.warn "This is a test"
-		Rails.logger.warn params[:id]
-		@citems=CalendarItem.where(user_id: params[:id])
-		Rails.logger.warn "This is a test2"
+		@citems=CalendarItem.where("user_id = ?", params[:id])
 	end
 end
