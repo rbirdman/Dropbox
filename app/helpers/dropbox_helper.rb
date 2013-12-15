5866f8796033f73ffa61737042bf3c84ade2fc20
Ryan Bird <rlbird22@gmail.com>
2013-12-14 22:34:12 -0700

    Make index.html dynamic

--- app/helpers/dropbox_helper.rb
+++ app/helpers/dropbox_helper.rb
@@ -1,2 +1,39 @@
 module DropboxHelper
+
+class Folder
+  def initialize(name, type, mod, perm)
+    @name = name
+    @type = type
+    @modified = mod
+    @permissions = perm
+  end
+  
+  def print()
+    puts @name
+    puts @type
+    puts @modified
+    puts @permissions
+  end
+  
+  def name
+    @name
+  end
+  def type
+    @type
+  end
+  def modified
+    @modified
+  end
+  def permissions
+    @permissions
+  end
+end
+
+  
+  def getFoldersFromDirectory(dir)
+    [Folder.new("bill.pdf","document","Today","public"),
+     Folder.new("john.pdf","document", "Tomorrow","shared"),
+     Folder.new("src/", "folder", "A week ago","private")]
+  end
+  
 end
