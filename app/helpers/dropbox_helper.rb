4a671daa84a5f737233a59593406c8bbcfee1cb1
Ryan Bird <rlbird22@gmail.com>
2013-12-15 15:04:07 -0700

    Make rest of files dynamic. Bottleneck/connector to database in methods in dropbox_helper.rb

--- app/helpers/dropbox_helper.rb
+++ app/helpers/dropbox_helper.rb
@@ -29,6 +29,44 @@ class Folder
   end
 end
 
+class ToDoItem
+  def initialize(titl, det, due, res, comp)
+    @title = titl
+    @details = det
+    @due_date = due
+    @resources = res
+    @completed = comp
+  end
+  
+  def title
+    @title
+  end
+  def details
+    @details
+  end
+  def due_date
+    @due_date
+  end
+  def resources
+    @resources
+  end
+  def completed
+    @completed
+  end
+end
+
+def getTodoItems(dirname)
+  tuple = ["www.ourteamwebsite.com", "www.youtube.com/watch?v=dQw4w9WgXcQ"]
+  [ToDoItem.new("CS 360 Reading","pgs. 100-135","11/20/13 3:00pm",[], false),
+     ToDoItem.new("CS 360 Team Meeting","Discuss Project", "3:30 pm", ["www.ourteamwebsite.com", "www.youtube.com/watch?v=dQw4w9WgXcQ"], true),
+     ToDoItem.new("ENGL 316 Write Essay", "5 pages", "11/19/13 12:00am",["How to Write.pdf"], true)]
+end
+
+def searchUser(search_val)
+  ["rbirdman", "Charles Xavier", "Magneto", "Chris Rock",
+   "Clark Kent", "Bruce Wayne", "Wolverine", "Aurora Monroe",
+    "Jean Grey"]
+end
   
   def getFoldersFromDirectory(dir)
     [Folder.new("bill.pdf","document","Today","public"),
