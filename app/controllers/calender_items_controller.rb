81301275c08a9fdbc137eb33fd08df7af3a13c31
bchase524 <bchase524@gmail.com>
2013-12-19 16:21:27 -0700

    Save and DeleteEvent methods made

--- app/controllers/calender_items_controller.rb
+++ app/controllers/calender_items_controller.rb
@@ -13,6 +13,7 @@ class CalenderItemsController < ApplicationController
 	    event_text = params[:event_text]
 	    event_start = params[:event_start]
 	    event_end = params[:event_end]
+	    CalendarItem.create(:name => event_text, :start_datetime => event_start, :end_datetime => event_end, :created_at => DateTime.now, :updated_at => DateTime.now)
 	  end
 	  
 	  
@@ -23,6 +24,8 @@ class CalenderItemsController < ApplicationController
 	  if current_user != nil
 	    id = current_user.id
 	    event_id = params[:event_id]
+		@value=CalendarItem.find(event_id)
+		@value.destroy
 	  end
 	  
 	  redirect_to "/calendar"
