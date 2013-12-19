a73a37589263866d58c80b2f3b1e13b7cb1bac05
Ryan Bird <rlbird22@gmail.com>
2013-12-18 17:56:39 -0700

    Save ToDo Item to database…Resource no longer required attribute, UserID is required. Using a default userID until that is working

--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -2,24 +2,30 @@ class DropboxController < ApplicationController
   def home
   end
   def todo
+    user = params[:user] || 7
     title = params[:title]
     details = params[:details]
     due_date = params[:duedate]
-    resources = params[:resources]
+    resources = params[:resources] || ""
     resourceAsLink = params[:link] || false
     
     Rails.logger.debug "Creating ToDo"
     
+    Rails.logger.debug "User: " + (user == nil ? "nil" : user.to_s())
     Rails.logger.debug "Title: " + (title == nil ? "nil" : title)
     Rails.logger.debug "Details: " + (details == nil ? "nil" : details)
     Rails.logger.debug "Due Date: " + (due_date == nil ? "nil" : due_date)
     Rails.logger.debug "Resources: " + (resources == nil ? "nil" : resources)
     Rails.logger.debug "link: " + (resourceAsLink == false ? "false" : "true")
     
-    if title == nil || details == nil || due_date == nil || resources == nil
+    if user == nil || title == nil || details == nil || due_date == nil
       Rails.logger.debug "Deny List"
     else
       Rails.logger.debug "Accept List"
+      ToDoItem.create(:user_id => user, :title => title, :details => details,
+      		:date => due_date, :resources => resources, :completed => false)
+      		#:date => due_date, :resources => resources, :link => resourceAsLink, :completed => false)
+      
     end
     
   end
