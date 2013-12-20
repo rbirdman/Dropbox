357e232bb91db231fc8ca1d47b219e9b878b9c28
bchase524 <bchase524@gmail.com>
2013-12-19 18:11:58 -0700

    Calendar Items now display correctly

--- app/controllers/calender_items_controller.rb
+++ app/controllers/calender_items_controller.rb
@@ -2,18 +2,23 @@ require 'builder'
 
 class CalenderItemsController < ApplicationController
 	def calendar
-		cidval=params[:id]
-		cidval||=-1
-		@citems=CalendarItem.where("user_id = ?", cidval)
+		if current_user != nil
+			id=current_user.id
+			@citems=CalendarItem.where("user_id = ?", id)
+		end
 	end
 	def saveEvent
 	  if current_user != nil
 	    id = current_user.id
+	    Rails.logger.warn id
+	    Rails.logger.warn "Hello"
 	    event_id = params[:event_id]
 	    event_text = params[:event_text]
 	    event_start = params[:event_start]
 	    event_end = params[:event_end]
-	    CalendarItem.create(:name => event_text, :start_datetime => event_start, :end_datetime => event_end, :created_at => DateTime.now, :updated_at => DateTime.now)
+	    @new_event=CalendarItem.create(:name => event_text,:user_id => id, :start_datetime => event_start, :end_datetime => event_end, :created_at => DateTime.now, :updated_at => DateTime.now)
+	    @new_event.save
+	    Rails.logger.warn "OKAY!"
 	  end
 	  
 	  
